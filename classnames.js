4ec85dc0052f76654c0525608717bb7fb21b98ee
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2024-03-25 10:06:07 +0500

    ...done

--- classnames.js
+++ classnames.js
@@ -2,14 +2,9 @@ import classNames from "classnames";
 
 const nightDay = () => {
   let className = "container";
-  const theme1 = "dark";
-  const theme2 = "light";
+  const theme = "dark";
 
-  className = classNames(
-    className,
-    { dark: theme1 === "dark" },
-    { light: theme2 === "light" }
-  );
+  className = classNames(className, { dark: theme === "dark" });
   console.log(className);
 };
 
